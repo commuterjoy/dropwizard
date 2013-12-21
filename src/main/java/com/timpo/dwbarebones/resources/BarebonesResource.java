f324cfefea74fba52fb50c12760d8663e128702b
Matt Chadburn <matt.chadburn@guardian.co.uk>
2013-12-21 21:52:03 +0000

    Play with cache headers

--- src/main/java/com/timpo/dwbarebones/resources/BarebonesResource.java
+++ src/main/java/com/timpo/dwbarebones/resources/BarebonesResource.java
@@ -1,20 +1,29 @@
 package com.timpo.dwbarebones.resources;
 
 import com.timpo.dwbarebones.models.Model;
+import com.google.common.base.Optional;
+import com.yammer.metrics.annotation.Timed;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.CacheControl;
 
 @Path("/")
-@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML})
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML})
 public class BarebonesResource {
 
   @GET
-  public Response index() {
-    return Response.ok(new Model("some-id", Integer.MAX_VALUE)).build();
+  @Timed
+  public Response index(@QueryParam("name") Optional<String> name) {
+    CacheControl cache = new CacheControl();
+    cache.setMaxAge(600);
+
+    return Response.ok().cacheControl(cache).build();
   }
 }
